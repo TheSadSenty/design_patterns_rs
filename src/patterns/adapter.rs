fdbaffe8efe986a34464090d1f910afbe2c99af3
Kasyanov Maxim <maxkasser90@gmail.com>
2024-06-06 01:58:11 +0300

    Reorganize adapter pattern

--- src/patterns/adapter.rs
+++ src/patterns/adapter.rs
@@ -1,5 +1,46 @@
-mod adapter;
 mod message;
 mod other_message;
 mod service;
 mod target;
+pub(crate) struct Adapter {
+    first: String,
+    second: String,
+}
+impl Adapter {
+    pub(crate) fn fit(msg: other_message::OtherMessage) -> Self {
+        let data: Vec<&str> = msg.full_name.split(' ').collect();
+        Adapter {
+            first: data[0].to_string(),
+            second: data[1].to_string(),
+        }
+    }
+}
+impl target::Target for Adapter {
+    fn get_first(&self) -> String {
+        self.first.clone()
+    }
+    fn get_second(&self) -> String {
+        self.second.clone()
+    }
+}
+#[cfg(test)]
+mod test {
+    use crate::patterns::adapter;
+    use crate::patterns::adapter::service::Service;
+    use crate::patterns::adapter::{message, other_message};
+
+    #[test]
+    fn adapter() {
+        let serv = Service {};
+        let msg1 = message::Message {
+            first_name: "One".to_string(),
+            second_name: "Message".to_string(),
+        };
+        serv.print_smth(Box::new(msg1));
+        let msg2 = other_message::OtherMessage {
+            full_name: "Other Message".to_string(),
+        };
+        let adapter = adapter::Adapter::fit(msg2);
+        serv.print_smth(Box::new(adapter));
+    }
+}
--- src/patterns/adapter/service.rs
+++ src/patterns/adapter/service.rs
@@ -1,27 +1,7 @@
 use crate::patterns::adapter::*;
-struct Service {}
+pub(crate) struct Service {}
 impl Service {
     pub fn print_smth(&self, msg: Box<dyn target::Target>) {
         println!("{} {}", msg.get_first(), msg.get_second())
-    }
-}
-#[cfg(test)]
-mod test {
-    use crate::patterns::adapter::service::Service;
-    use crate::patterns::adapter::{adapter, message, other_message};
-
-    #[test]
-    fn adapter() {
-        let serv = Service {};
-        let msg1 = message::Message {
-            first_name: "One".to_string(),
-            second_name: "Message".to_string(),
-        };
-        serv.print_smth(Box::new(msg1));
-        let msg2 = other_message::OtherMessage {
-            full_name: "Other Message".to_string(),
-        };
-        let adapter = adapter::Adapter::fit(msg2);
-        serv.print_smth(Box::new(adapter));
     }
 }
